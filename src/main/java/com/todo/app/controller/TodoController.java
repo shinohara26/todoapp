c4c3a9db2232c343897880700b9d5590bb98f05b
shinohara <taichi.shinohara.au@jtsnet.co.jp>
2026-06-18 17:26:14 +0900

    Update:追加画面実装 　　　　　 追加画面の画面遷移 　　　　　　子タスク詳細画面の実装
    Fix:完了済みの動作不良改善

--- src/main/java/com/todo/app/controller/TodoController.java
+++ src/main/java/com/todo/app/controller/TodoController.java
@@ -24,7 +24,7 @@ import com.todo.app.mapper.TodoMapper;
 
 @Controller
 public class TodoController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(TodoController.class);
 
 	@Autowired
@@ -34,49 +34,57 @@ public class TodoController {
 	@Autowired
 	CategoryMapper categoryMapper;
 
-	@RequestMapping(value="/")
+	@RequestMapping(value = "/")
 	public String index(Model model) {
 		logger.info("アクセス: /");
 		logger.info("ログ出力aaa");
 
-//		List<Todo> list = todoMapper.selectAll();
+		//		List<Todo> list = todoMapper.selectAll();
 
 		List<Todo> list = todoMapper.selectIncomplete();
 		List<Todo> doneList = todoMapper.selectComplete();
-		
+
+		List<Priority> priorityList = priorityMapper.selectAll();
+		List<Category> categoryList = categoryMapper.selectAll();
+
 		if (false) {
 			logger.debug("list:" + list.size() + "件");
 			logger.debug("doneList:" + doneList.size() + "件");
 		}
-		
-		model.addAttribute("todos",list);
-		model.addAttribute("doneTodos",doneList);
+
+		model.addAttribute("todos", list);
+		model.addAttribute("doneTodos", doneList);
+		model.addAttribute("priorityList", priorityList);
+		model.addAttribute("categoryList", categoryList);
 
 		return "index";
 	}
-	
-	@RequestMapping(value="/add")
+
+	@RequestMapping(value = "/add")
 	@ResponseBody
 	public Map<String, Object> add(@Valid Todo todo, BindingResult result) {
-	    logger.info("アクセス: /add");
+		logger.info("アクセス: /add");
 
-	    Map<String, Object> response = new HashMap<>();
+		Map<String, Object> response = new HashMap<>();
 
-	    if (result.hasErrors()) {
-	        response.put("success", false);
-	        response.put("errors", result.getAllErrors());
-	        return response;
-	    }
+		if (result.hasErrors()) {
+			response.put("success", false);
+			response.put("errors", result.getAllErrors());
+			return response;
+		}
 
-	    todoMapper.add(todo);
-	    response.put("success", true);
-	    response.put("todo", todo);
-	    return response;
+		todoMapper.add(todo);
+
+		logger.info("追加後 id = " + todo.getId());
+
+		response.put("success", true);
+		response.put("id", todo.getId());
+		return response;
 
 	}
 
-	@RequestMapping(value="/update")
-//	@ResponseBody
+	@RequestMapping(value = "/update")
+	//	@ResponseBody
 	public String update(Todo todo) {
 		logger.info("アクセス: /update");
 		logger.info("id=" + todo.getId());
@@ -87,31 +95,44 @@ public class TodoController {
 		logger.info("work_plan_day=" + todo.getWork_plan_day());
 		logger.info("memo=" + todo.getMemo());
 
-		
 		todoMapper.update(todo);
-		
+
 		return "redirect:/detail?id=" + todo.getId();
 	}
 
-	@RequestMapping(value="/delete")
+	@RequestMapping(value = "/delete")
 	@ResponseBody
 	public void delete() {
 		logger.info("アクセス: /delete");
-		
+
 		todoMapper.delete();
 	}
 
-	@RequestMapping(value="/detail")
+	@RequestMapping(value = "/detail")
 	public String detail(Integer id, Model model) {
 		logger.info("アクセス: /detail");
-		
+
 		Todo todo = todoMapper.selectById(id);
 		List<Priority> priorityList = priorityMapper.selectAll();
 		List<Category> categoryList = categoryMapper.selectAll();
-		
+
 		model.addAttribute("todo", todo);
 		model.addAttribute("priorityList", priorityList);
 		model.addAttribute("categoryList", categoryList);
 		return "detail";
+	}
+
+	@RequestMapping(value = "/child/detail")
+	public String childDetail(Integer id, Model model) {
+		logger.info("アクセス: /child/detail");
+
+		Todo todo = todoMapper.selectById(id);
+		List<Priority> priorityList = priorityMapper.selectAll();
+		List<Category> categoryList = categoryMapper.selectAll();
+
+		model.addAttribute("todo", todo);
+		model.addAttribute("priorityList", priorityList);
+		model.addAttribute("categoryList", categoryList);
+		return "child_detail";
 	}
 }
--- src/main/resources/static/js/todo.js
+++ src/main/resources/static/js/todo.js
@@ -1,105 +1,118 @@
 /**
  *
  */
-$(function(){
+$(function() {
 
-//完了済みの個数取得・表示
-  let doneCount = $('#donetodes').children("tr").length;
-  $('#done_count').text(doneCount);
+    //完了済みの個数取得・表示
+    let doneCount = $('#donetodes').children("tr").length;
+    $('#done_count').text(doneCount);
 
-//更新処理
-$('.todo input').change(function(){
-	const todo = $(this).parents('.todo');
-	const id = todo.find('input[name="id"]');
-	const title = todo.find('input[name="title"]');
-	const timeLimit = todo.find('input[name="work_plan_day"]');
-	const isDone = todo.find('input[name="done_flg"]').prop("checked");
-	let doneFlg;
-	if(isDone == true) {
-	  doneFlg = true;
-	}else{
-	  doneFlg = false;
-	}
+    //更新処理
+    $('.todo input').change(function() {
+        const todo = $(this).parents('.todo');
+        const id = todo.find('input[name="id"]');
+        const title = todo.find('input[name="title"]');
+        const timeLimit = todo.find('input[name="work_plan_day"]');
+        const isDone = todo.find('input[name="done_flg"]').prop("checked");
+        let doneFlg;
+        if (isDone == true) {
+            doneFlg = true;
+        } else {
+            doneFlg = false;
+        }
 
-	const params = {
-		id : id.val(),
-		title : title.val(),
-		work_plan_day : timeLimit.val(),
-		done_flg : doneFlg
-	}
-	$.post("/update",params);
+        const params = {
+            id: id.val(),
+            title: title.val(),
+            work_plan_day: timeLimit.val(),
+            done_flg: doneFlg
+        }
+        $.post("/update", params);
 
-    //完了ボタンを押した際の処理
-    doneCount =  $('#done_count').text();
+        //完了ボタンを押した際の処理
+        doneCount = $('#done_count').text();
 
-	if($(this).prop('name') == "done_flg"){
-	  if(isDone == true){
-	    $(todo).appendTo('#donetodes');
-	    todo.find('input[name="title"]').css('text-decoration','line-through')
-	    todo.find('input[name="work_plan_day"]').hide();
-	    doneCount ++;
-	  }else{
-	    $(todo).appendTo('#todes');
-	    todo.find('input[name="title"]').css('text-decoration','none')
-	    todo.find('input[name="work_plan_day"]').show()
-	    doneCount --;
-	  }
+        if ($(this).prop('name') == "done_flg") {
+            if (isDone == true) {
+                $(todo).appendTo('#donetodes');
+                todo.find('input[name="title"]').css('text-decoration', 'line-through')
+                todo.find('input[name="work_plan_day"]').hide();
+                doneCount++;
+            } else {
+                $(todo).appendTo('#todes');
+                todo.find('input[name="title"]').css('text-decoration', 'none')
+                todo.find('input[name="work_plan_day"]').show()
+                doneCount--;
+            }
 
-	  $("#done_count").text(doneCount);
-	}
+            $("#done_count").text(doneCount);
+        }
 
 
-})
-
-//完了済みタスク表示/非表示切り替え
-$('.button_for_show').click(function(){
-    let showState = $('#done_table').css('display');
-    if(showState == "none") {
-        $('#done_table').show();
-        $(this).css({ transform: ' rotate(225deg)','bottom':'-4px' });
-    }else{
-        $('#done_table').hide();
-        $(this).css({ transform: ' rotate(45deg)','bottom':'4px' });
-    }
-})
-
-//追加処理
-$('#add').click(function() {
-    const params = $('#add_form').serializeArray();
-    $.post("/add",params).done(function(json){
-        const clone = $('#todes tr:first').clone(true);
-        clone.find('input[name="id"]').val(json.id);
-        clone.find('input[name="title"]').val(json.title);
-        clone.find('input[name="work_plan_day"]').val(json.work_plan_day);
-        $('#todes').append(clone[0]);
     })
-})
 
-//削除処理
-$('#delete').click(function(){
-    $.post("/delete").done(function(){
-        $('#donetodes').empty();
-        $('#done_count').text(0);
+    //完了済みタスク表示/非表示切り替え
+    $('.button_for_show').click(function() {
+        let showState = $('#done_table').css('display');
+        if (showState == "none") {
+            $('#done_table').show();
+            $(this).css({ transform: ' rotate(225deg)', 'bottom': '-4px' });
+        } else {
+            $('#done_table').hide();
+            $(this).css({ transform: ' rotate(45deg)', 'bottom': '4px' });
+        }
     })
-})
 
-//詳細画面へ
+    //追加処理
+    $('#add').click(function() {
+        const params = $('#add_form').serialize();
 
-$(".detail-btn").on("click", function() {
-	const id = $(this).data("id");
-    location.href = "/detail?id=" + id;
-});
+        $.ajax({
+            url: "/add",
+            type: "POST",
+            data: params,
+            dataType: "json"
+        }).done(function(json) {
+            console.log("add response =", json);
 
-// 保存ボタン
-$("#saveBtn").on("click", function() {
-	// checkbox の状態を hidden に入れて送信
-    $("#doneFlgValue").val($("#doneFlgCheck").prop("checked"));
-    $("#detailForm").submit();
-});
+            if (json.success) {
+                location.href = "/detail?id=" + json.id;
+            }
 
-// 戻るボタン
-$("#backBtn").on("click", function() {
-	location.href = $(this).data("url");
-});
+        });
+
+        $('#cancelAdd').click(function() {
+            $('#modal').modal('hide');
+            $('#add_form')[0].reset();
+        });
+    });
+
+
+    //削除処理
+    $('#delete').click(function() {
+        $.post("/delete").done(function() {
+            $('#donetodes').empty();
+            $('#done_count').text(0);
+        })
+    })
+
+    //詳細画面へ
+
+    $(".detail-btn").on("click", function() {
+        const id = $(this).data("id");
+        location.href = "/detail?id=" + id;
+    });
+
+    // 保存ボタン
+    $("#saveBtn").on("click", function() {
+        // checkbox の状態を hidden に入れて送信
+        $("#doneFlgValue").val($("#doneFlgCheck").prop("checked"));
+        $("#detailForm").submit();
+    });
+
+    // 戻るボタン
+    $("#backBtn").on("click", function() {
+        location.href = $(this).data("url");
+    });
 
 });
